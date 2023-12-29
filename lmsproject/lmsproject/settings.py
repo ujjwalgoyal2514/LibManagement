7eaa53618dc6545a433ba8563b5b61a396694afe
ujjwal <ujjwalgoyal2514@gmail.com>
2023-12-29 13:36:05 +0530

    deploy

--- lmsproject/lmsproject/settings.py
+++ lmsproject/lmsproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n2!_#lu&zmfmf+$tpia$%0k#kehy(p&q1m91^f)6@a9zps+@qg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/lmsapp/static/'
+STATICFILES_DIRS=os.path.join(BASE_DIR,'lmsapp/static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','lmsapp/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
